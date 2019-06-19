7de0c38db3c2ef5e25e7dd06cc15ca69f487a5ae
Jessica Sarlin <jessica.sarlin@gmail.com>
2019-06-19 09:36:27 +0300

    3.21 app uses mongo app

---
+++ build/precache-manifest.baa79f280110b7a26d7dc43a26c28d7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b04e98c59a1469c9aa307e64280c30b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35a68f6adfb2303ac56b",
+    "url": "/static/css/main.16606489.chunk.css"
+  },
+  {
+    "revision": "6a19a0beea039a280f26",
+    "url": "/static/js/2.3c62b441.chunk.js"
+  },
+  {
+    "revision": "35a68f6adfb2303ac56b",
+    "url": "/static/js/main.3236d0e6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
